76bf5c76a030f1db3732e2b87880725baff38295
cenxun111 <32427222+cenxun111@users.noreply.github.com>
2021-08-12 20:50:59 +0800

    Delete mac8.12 directory

--- mac8.12/client/src/redux-toolkit/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import loginReducer from '../redux-toolkit/authSlice'
-import postReducer from '../redux-toolkit/postSlice'
-export const store = configureStore({
-  reducer: {
-      login: loginReducer,
-      posts: postReducer
-  },
-})
--- mac8.12/server/routes/wordRouter.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-const wordCtrl = require('../controllers/wordCtrl')
-
-router.post('/createWord',wordCtrl.createWord)
-router.get('/user_words/:username',wordCtrl.getUserWords)
-router.delete('/user_words/:id',wordCtrl.deleteWord)
-
-module.exports = router
